41658896016e62f78c3498ee2846f6e67b13375a
Mostafa <atefmostafa356@gmail.com>
2024-02-17 22:13:01 +0200

    connect project with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:ecommerce/core/utils/classes/app_router.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const EcommerceApp());
 }
 
